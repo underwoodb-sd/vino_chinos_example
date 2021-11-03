aa92e677b072b573a9bee7c35071d59f46b86ead
Brandon Underwood <brandon.underwood@netapp.com>
2021-11-03 07:44:25 -0400

    Initial commit

---
+++ test_vino_chinos.py
@@ -0,0 +1,27 @@
+"""Test module for vino_chinos.py
+
+Features:
+1. Convert an integer to a string, based on the vino chinos algorithm.
+2. Map this converter to a list of numbers
+3. Print out mapped list
+
+Test Cases:
+1. Vino chinos converter:
+    - Can call
+    - Convert regular number to string
+    - Convert number divisible by 3 to "Vino"
+    - Convert number divisible by 5 to "Chinos"
+    - Convert number divisible by 3 and 5 to "Vino Chinos"
+2. List mapper:
+    - Can call
+    - Return converted list when given an integer list
+3. Printer:
+    - Can call
+    - Given an empty list, print an error message
+    - Given a list, prints expected values
+
+"""
+
+
+def test_nothing():
+    pass
